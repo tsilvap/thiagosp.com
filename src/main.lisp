2ef9d44537ddace30fa6f6e42d02da2a6c653256
Thiago da Silva Pinto <thiagodasilva@protonmail.com>
2024-07-30 06:19:04 -0300

    Regenerate static page on each visit in development server

--- src/main.lisp
+++ src/main.lisp
@@ -1,26 +1,31 @@
 (in-package :com.thiagosp)
 
-(defmacro generate-page (system-relative-dest-path &body body)
-  (with-gensyms (dest-path f)
-    `(let ((,dest-path (asdf:system-relative-pathname
-                        "thiagosp" ,system-relative-dest-path)))
-       (ensure-directories-exist ,dest-path)
-       (with-open-file (,f ,dest-path
+(defmacro generate-page ((&key destination) &body body)
+  (with-gensyms (dest-pathname f)
+    `(let ((,dest-pathname (asdf:system-relative-pathname
+                            "thiagosp" ,destination)))
+       (ensure-directories-exist ,dest-pathname)
+       (with-open-file (,f ,dest-pathname
                            :direction :output
                            :if-exists :supersede
                            :if-does-not-exist :create)
-         (write-sequence (progn ,@body) ,f)))))
+         (write-sequence (progn ,@body) ,f))
+       ,@body)))
 
 (defun generate-site (sorted-movies)
   "Generates all the files for the static website."
-  (generate-page "dist/index.html" (home-page))
+  (generate-page (:destination "dist/index.html")
+    (home-page))
 
-  (generate-page "dist/notes.html" (notes-page))
+  (generate-page (:destination "dist/notes.html")
+    (notes-page))
   (loop for note in (notes:get-all-notes)
-        do (generate-page (format nil "dist/notes/~A.html" (notes:slug note))
+        do (generate-page (:destination
+                           (format nil "dist/notes/~A.html" (notes:slug note)))
              (note-page (notes:slug note))))
 
-  (generate-page "dist/in-theaters.html" (in-theaters-page sorted-movies)))
+  (generate-page (:destination "dist/in-theaters.html")
+    (in-theaters-page sorted-movies)))
 
 ;;; Set up simple Hunchentoot server for development only. In
 ;;; production, we serve the static website from an external web
@@ -49,15 +54,19 @@
 (defparameter *sorted-movies* (get-sorted-movies))
 
 (easy-routes:defroute root ("/" :method :get) ()
-  (home-page))
+  (generate-page (:destination "dist/index.html")
+    (home-page)))
 
 (easy-routes:defroute notes ("/notes" :method :get) ()
-  (notes-page))
+  (generate-page (:destination "dist/notes.html")
+    (notes-page)))
 
 (easy-routes:defroute note ("/notes/:note-slug" :method :get) ()
-  (note-page note-slug))
+  (generate-page (:destination (format nil "dist/notes/~A.html" note-slug))
+    (note-page note-slug)))
 
 (easy-routes:defroute in-theaters ("/in-theaters" :method :get) ()
-  (in-theaters-page *sorted-movies*))
+  (generate-page (:destination "dist/in-theaters.html")
+    (in-theaters-page *sorted-movies*)))
 
 (generate-site *sorted-movies*)
